938d944df5847ab558e13e9db8f8dfef76eba92b
Sayeeda Tasmia <smohammednasir@hawk.iit.edu>
2019-12-30 15:03:12 -0600

    deleted

--- Rich Internet Application/Demo/class13demo/webpack-babel-react-basic/src/Headline.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Headline extends React.Component {
-    render(){
-        return <h1>Hello {this.props.name}!</h1>;
-    }
-}
-
-export default Headline;
--- Rich Internet Application/Demo/class14demo/NoRouterDemo/src/NavItem.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function NavItem(props) {
-    return (
-        <a href="#" onClick={() => props.onPageChange(props.page)}>{props.text}</a>
-    );
-}
-
-export default NavItem;
